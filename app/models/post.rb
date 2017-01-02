d97b23a3884ee4848a93042a2ef3ac08523c174c
JAMPANA VARMA <JAMPANA.VARMA49@GMAIL.COM>
2017-01-02 04:22:28 -0800

    #7 - Form added for tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,14 @@ class Post < ApplicationRecord
   def posted_on
     created_at.strftime "Posted on %B %d, %Y"
   end
+
+  def str_tags=(input_tags)
+    self.tags = input_tags.split(/\W+/).map do |tag|
+      Tag.find_or_create_by(name: tag.downcase)
+    end
+  end
+
+  def str_tags
+    tags.map(&:name).join(' ')
+  end
 end
